86bd64d4ebe11d24352efa3f11d297b6f42eb69b
wangz <1057708856@qq.com>
2021-07-16 14:47:05 +0800

    创意工坊基本

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,182 +1,150 @@
 <template>
   <div class="mainpage">
-      <div class="choosebox" :class="{show:ifchoose}">
-        <p class="title">PUSH THE BOX!</p>
-        <button class="choosebtn" @click="chooselevel">选择关卡</button>
-        <button class="choosebtn" @click="toWorkshop">创意工坊</button>
-        <button class="choosebtn" @click="toCreate">创建地图</button>
+    <div class="choosebox" :class="{ show: ifchoose }">
+      <p class="choosebox-title">PUSH THE BOX!</p>
+      <button class="choosebtn" @click="chooseLevel">选择关卡</button>
+      <button class="choosebtn" @click="toWorkshop">创意工坊</button>
+      <button class="choosebtn" @click="toCreate">创建地图</button>
+    </div>
+    <!--弹窗-->
+    <div class="out" :class="{ show: ifchoose }">
+      <p class="out-title">选择关卡</p>
+      <div class="level-content">
+        <button v-for="(item, index) in officials" :key="index" class="btn" @click="levelIs(index + 1)" :class="{ active: level == index + 1 }">第{{ index + 1 }}关</button>
       </div>
-      <!--弹窗-->
-      <div class="out" :class="{show:ifchoose}">
-          <p class="title2">选择关卡</p>
-          <div class="level-content">
-              <button 
-              v-for="(item,index) in officials"
-               :key="index" class="btn" @click="levelis(index+1)"
-                :class="{active:level == index + 1 }">第{{index + 1}}关</button>
-          </div>
-          <div class="level-btn">
-              <button class="btn btn-ok" @click="toGame">确定</button>
-              <button class="btn btn-no" @click="chooselevel">返回</button>
-          </div>
+      <div class="level-btn">
+        <button class="btn btn-ok" @click="toGame">确定</button>
+        <button class="btn btn-no" @click="chooseLevel">返回</button>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import {official} from "../assets/js/level"
+import { official } from '../assets/js/level'
 export default {
-    data:function(){
-        return {
-            ifchoose:false,
-            officials:[],
-            level:""
-        }
-    },
-methods:{
+  data: function() {
+    return {
+      ifchoose: false,
+      officials: [],
+      level: ''
+    }
+  },
+  methods: {
     //跳转创建地图
-    toCreate(){
-        this.$router.push("/create")
+    toCreate() {
+      this.$router.push('/create')
     },
     //选择关卡按钮弹出
-    chooselevel(el){
-          this.ifchoose = !this.ifchoose 
-          this.level = ""
+    chooseLevel(el) {
+      this.ifchoose = !this.ifchoose
+      this.level = ''
     },
     //选择关卡
-    levelis(num){
-        this.level = num
+    levelIs(num) {
+      this.level = num
     },
     //跳转游戏页面
-    toGame(){
-        if(this.level === ""){
-             this.$message({
-             message: '请选择关卡后再确定',
-             type: 'warning'
-        });
-        }else{
-            this.$router.push({path:"game",query:{level:this.level}})
-        }
+    toGame() {
+      if (this.level === '') {
+        this.$message({
+          message: '请选择关卡后再确定',
+          type: 'warning'
+        })
+      } else {
+        this.$router.push({ path: 'game', query: { level: this.level } })
+      }
     },
     //跳转创意工坊
-    toWorkshop(){
-        this.$router.push('/workshop')
+    toWorkshop() {
+      this.$router.push('/workshop')
     }
-},
-created(){
+  },
+  created() {
     this.officials = official
-}
+  }
 }
 </script>
 
 <style>
+@import '../assets/css/animation.css';
 /*首页*/
-.mainpage{
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-    position: relative;
+.mainpage {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
 }
-.choosebox{
-    width: 100vw;
-    height: 100vh;
-    background: rgb(149, 229, 240);
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    transition: all .5s;
+.choosebox {
+  width: 100vw;
+  height: 100vh;
+  background: rgb(149, 229, 240);
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.5s;
 }
-.choosebtn,.title{
-    width: 50%;
-    height: 50px;
-    text-align: center;
-    margin-top: 10px;
+.choosebtn,
+.choosebox-title {
+  width: 50%;
+  height: 50px;
+  text-align: center;
+  margin-top: 10px;
 }
-.choosebtn{
-    border-radius: 10px;
-    cursor: pointer;
-    border: 1px solid rgb(2, 20, 31);
+.choosebtn {
+  border-radius: 10px;
+  cursor: pointer;
+  border: 1px solid rgb(2, 20, 31);
 }
-.title{
-    width: 100%;
-    margin-top: 30px;
-    font-size: 20px;
-    animation: change 5s infinite;
-    position: relative;
-    top: -20vh;
-}
-@keyframes change {
-  0% {
-    color: #c89c4b;
-  }
-  25% {
-    color: #C779D0;
-  }
-  50%{
-    color: rgb(194, 66, 66);
-  }
-  75%{
-      color: rgb(110, 143, 18);
-  }
-  100%{
-      color: rgb(48, 190, 60);
-  }
-}
-.show{
-    transform: translate(0,-100vh);
-    transition: all 1s;
+.show {
+  transform: translate(0, -100vh);
+  transition: all 1s;
 }
 /*弹窗 */
-.out{
-    width: 100vw;
-    height: 100vh;
-     background: rgb(149, 229, 240);
-    transition: all .5s;
-    padding-top: 50px;
+.out {
+  width: 100vw;
+  height: 100vh;
+  background: rgb(149, 229, 240);
+  transition: all 0.5s;
+  padding-top: 50px;
 }
-.title2{
-    font-size: 30px;
-    animation: change 5s infinite;
-    margin: 0 auto 50px;
-    text-align: center;
+.level-content {
+  width: 80%;
+  height: 50vh;
+  margin: 0 auto 5px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-content: flex-start;
 }
-.level-content{
-    width: 80%;
-    height: 50vh;
-    margin: 0 auto 5px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    align-content: flex-start;
+.level-btn {
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto 10px;
 }
-.level-btn{
-    width: 80%;
-    display: flex;
-    justify-content: center;
-    margin: 0 auto 10px;
+.btn {
+  width: 30%;
+  height: 50px;
+  margin-left: 5px;
+  margin-top: 5px;
+  border: none;
+  cursor: pointer;
+  color: #ffffff;
+  background: rgb(16, 162, 219);
+  border-radius: 10px;
 }
-.btn{
-    width: 30%;
-    height: 50px;
-    margin-left: 5px;
-    margin-top: 5px;
-    border:none;
-    cursor: pointer;
-    color: #ffffff;
-    background: rgb(16, 162, 219);
-    border-radius: 10px;
+.btn-ok {
+  background: rgb(16, 162, 219);
+  margin-right: 10px;
 }
-.btn-ok{
-   background: rgb(16, 162, 219);
-   margin-right: 10px;
+.btn-no {
+  background: rgb(255, 46, 46);
+  margin-left: 10px;
 }
-.btn-no{
-    background: rgb(255, 46, 46);
-    margin-left: 10px;
-}
-.active{
+.active {
   background: rgb(26, 23, 179);
 }
 </style>
--- src/views/Workshop.vue
+++ src/views/Workshop.vue
@@ -1,13 +1,82 @@
 <template>
-  
+  <div class="workshop">
+    <p class="workshop-title out-title">创意工坊</p>
+    <div class="workshop-content">
+      <div class="player" v-for="(item,index) in mapValue" :key="index">
+          <p>{{item.name}}</p>
+          <p>{{item.author}}</p>
+          <p>{{item.date}}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { request } from '@/network/request'
 export default {
-
+  data: function() {
+    return {
+        //地图数据
+        mapValue:[
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"},
+        {name:"我的地图",author:"张三",date:"2021-7-16"}],
+    }
+  },
+  methods: {
+    //获取地图数据
+    getPlayerMap() {
+      request({
+        url: 'map/list',
+        method: 'get'
+      }).then(res => {
+        res.data.forEach(el => {
+            this.mapValue.push(el)
+        });
+      }).catch(err=>{
+          this.$message({
+          message: '获取地图失败：' + err,
+          type: 'error'
+        });
+      })
+    }
+  },
+  created() {
+    this.getPlayerMap()
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+@import '../assets/css/animation.css';
+.workshop {
+  width: 100vw;
+  padding-top: 30px;
+  background: #95d52f;
+}
+.workshop-content{
+    width: 80%;
+    margin: 20px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+.player{
+    width: 100%;
+    height: 70px;
+    margin: 10px 0;
+    background: #339933;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: #ffffff;
+    font-size: 15px;
+}
 </style>
