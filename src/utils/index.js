9047349bfd73ddba4e130373015f6a54210a84ad
yuanyuanna <yuan1119@163.com>
2021-08-10 15:08:17 +0800

    fix: 冲突

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,26 +3,6 @@ export const deepCloneObj = obj => {
   return JSON.parse(JSON.stringify(obj))
 }
 
-<<<<<<< HEAD
-export const isArr = (arr) => {
-  return Object.prototype.toString.call(arr) === '[object Array]'
-}
-
-export const deepClone = (quote) => {
-  if (typeof quote !== 'object' || quote === null || quote === 'null') return quote;
-  const result = isArr(quote) ? [] : {};
-  for (const key in quote) {
-    if (quote.hasOwnProperty(key)) {
-      if (quote[key] instanceof Object) {
-        result[key] = deepClone(quote[key])
-      } else {
-        result[key] = quote[key];
-      }
-    }
-  }
-  return result;
-}
-=======
 // 深拷贝二维数组
 export const deepClone2Arr = arr => {
   let out = []
@@ -40,4 +20,3 @@ export const deepClone2Arr = arr => {
 export const isEmptyObject = (data) => {
   return JSON.stringify(data) == '{}'
 }
->>>>>>> master
