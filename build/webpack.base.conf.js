f78fa7070f60124fa4ffc2ba5717c56e13a4997b
aring <1303340995@qq.com>
2021-08-16 15:36:48 +0800

    feat: 修改转码图片大小限制

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -47,7 +47,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 51200,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
