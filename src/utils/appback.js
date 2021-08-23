054b67f11dddfa1c9e7b3dc96e9620f0a1e0ca55
aring <1303340995@qq.com>
2021-08-23 09:10:05 +0800

    fix: 修复需要点击三下返回键才能退出游戏的问题

--- src/utils/appback.js
+++ src/utils/appback.js
@@ -1,29 +1,25 @@
 import { Toast } from 'vant'
 // 处理hbuilder打包app之后点击手机返回键直接退出app的问题
+let first = null
 document.addEventListener('plusready', function() {
-  var webview = plus.webview.currentWebview()
+  const webview = plus.webview.currentWebview()
   plus.key.addEventListener('backbutton', function() {
     webview.canBack(function(e) {
       if (e.canBack) {
         webview.back()
       } else {
         // 首页返回键处理，1秒内，连续两次按返回键，则退出应用；
-        var first = null
-        plus.key.addEventListener('backbutton',function() {
-            if (!first) {
-              first = new Date().getTime()
-              Toast('再按一次退出应用')
-              setTimeout(function() {
-                first = null
-              }, 1000)
-            } else {
-              if (new Date().getTime() - first < 1500) {
-                plus.runtime.quit()
-              }
-            }
-          },
-          false
-        )
+        if (!first) {
+          first = new Date().getTime()
+          Toast('再按一次退出游戏')
+          setTimeout(function() {
+            first = null
+          }, 1000)
+        } else {
+          if (new Date().getTime() - first < 1500) {
+            plus.runtime.quit()
+          }
+        }
       }
     })
   })
