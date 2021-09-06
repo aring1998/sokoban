44fbcdf2c5a84f34b14a983e1472d45eaaa92c8c
aring <1303340995@qq.com>
2021-09-06 09:56:47 +0800

    perf: 渲染游戏单元格方法优化

--- src/components/game-content/components/GameCell.vue
+++ src/components/game-content/components/GameCell.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import { staticMapRander, acitveMapRander } from './game-cell'
 
 export default {
   props: {
@@ -19,26 +20,9 @@ export default {
     randerClass() {
       let res = ''
       // 静止层渲染
-      const staticMapRander = {
-        0: () => { res = 'wall' },
-        4: () => { res = 'end' },
-        5: () => { res = 'spikeweed' },
-        6: () => { res = 'fire' },
-        8: () => { res = 'toadstool' },
-        9: () => { res = 'spring' },
-        10: () => { res = 'single-portal-entry' },
-        11: () => { res = 'single-portal-exit' },
-        12: () => { res = 'beer' },
-        13: () => { res = 'nectar' },
-      }
-      if (staticMapRander[this.staticValue]) staticMapRander[this.staticValue]()
+      if (staticMapRander[this.staticValue]) res = staticMapRander[this.staticValue]
       // 活动层渲染
-      const acitveMapRander = {
-        2: () => { res += ' player' },
-        3: () => { res += ' box' },
-        7: () => { res += ' ice-box' }
-      }
-      if (acitveMapRander[this.activeValue]) acitveMapRander[this.activeValue]()
+      if (acitveMapRander[this.activeValue]) res += acitveMapRander[this.activeValue]
       return res
     }
   }
