0f90d2cfc02bcfa7298612b5dc4d7c8be710c8f3
aring <1303340995@qq.com>
2022-01-07 16:56:42 +0800

    fix: 修复未正确弹出toast的问题

--- src/utils/appback.js
+++ src/utils/appback.js
@@ -1,8 +1,8 @@
 // 处理hbuilder打包app之后点击手机返回键直接退出app的问题
 let first = null
-document.addEventListener('plusready', function() {
+document.addEventListener('plusready', function () {
   const webview = plus.webview.currentWebview()
-  plus.key.addEventListener('backbutton', function() {
+  plus.key.addEventListener('backbutton', function () {
     // 获取当前路由
     const routes = getCurrentPages()
     const currentPath = routes[routes.length - 1].route
@@ -10,8 +10,8 @@ document.addEventListener('plusready', function() {
       // 首页返回键处理，1秒内，连续两次按返回键，则退出应用
       if (!first) {
         first = new Date().getTime()
-        uniq.showToast('😫再按一次退出游戏')
-        setTimeout(function() {
+        uni.showToast({ title: '😫再按一次退出游戏', icon: 'none' })
+        setTimeout(function () {
           first = null
         }, 1000)
       } else {
